9467dc33f129423c17e134d784b728c64653e593
gr423_niaal <nikulshin.senia@yandex.ru>
2024-04-30 10:02:10 +0700

    Initial commit

--- js/main.js
+++ js/main.js
@@ -16,6 +16,29 @@ let app = new Vue({
                 variantColor: 'blue'
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        cart: 0,
+        methods: {
+            addToCart() {
+                this.cart += 1
+            }
+        },
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+
+
+
     }
 })
